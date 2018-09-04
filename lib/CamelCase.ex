bc16c3e1368fdeaf868b31c182711e78a6a366f3
Ruthraiah Thulasi <ruthraiah.thulasi@gmail.com>
2018-09-04 16:08:30 +0530

    With case instead of method

--- lib/CamelCase.ex
+++ lib/CamelCase.ex
@@ -1,20 +1,17 @@
 defmodule CamelCase do
 
-  defp to_camel_case(str , index) when index ==0 do
-     str
-  end
-
-  defp to_camel_case(str , _ )  do
-    String.capitalize(str)
-  end
-
   def to_camel_case(str) do
 
     Regex.split(~r/-|[_]/, str)
     |> Enum.with_index
     |> Enum.map( fn {str,index} ->
-        to_camel_case(str,index) end)
+       case index do
+         0 -> str
+         _ -> String.capitalize(str)
+      end
+    end)
     |> Enum.join()
-  
+
   end
+  
 end
